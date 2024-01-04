842e24d9f019af0c5954fc9a12f10a46eae6641f
Abhinav K <48765255+akomand3012@users.noreply.github.com>
2024-01-04 17:16:43 -0600

    Added Unfinished Arduino Support

---
+++ Software/CapSenseTest_Controller/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lfrwp_init.a -l"C:/Users/Abhinav\ Komanduri/OneDrive\ -\ University\ of\ Arkansas/Research/AESIR\ Lab/CapSense/Software/CapSenseTest_Controller/captivate/BASE/libraries/captivate_fr2676_family.lib" -l"C:/Users/Abhinav\ Komanduri/OneDrive\ -\ University\ of\ Arkansas/Research/AESIR\ Lab/CapSense/Software/CapSenseTest_Controller/driverlib/MSP430FR2xx_4xx/libraries/driverlib_fr2676_family.lib" -l"C:/Users/Abhinav\ Komanduri/OneDrive\ -\ University\ of\ Arkansas/Research/AESIR\ Lab/CapSense/Software/CapSenseTest_Controller/mathlib/libraries/QmathLib.lib" -l"C:/Users/Abhinav\ Komanduri/OneDrive\ -\ University\ of\ Arkansas/Research/AESIR\ Lab/CapSense/Software/CapSenseTest_Controller/mathlib/libraries/IQmathLib.lib" -llibc.a
+
